7676056aeea0fd842754cc45578e73a1fee50ba7
edward.yin <edward.yinxp@gmail.com>
2025-02-23 15:14:08 +0800

    penguin

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,8 +3,8 @@ fn greet() {
     let soutuh_germ = "Grüß Gott!";
     let chinese = "你好";
     let eng = "hello, world";
-    let region = [soutuh_germ, chinese, eng];
-    for region in region.iter() {
+    let regions = [soutuh_germ, chinese, eng];
+    for region in regions.iter() {
         println!("{}", &region);
     }
 }
